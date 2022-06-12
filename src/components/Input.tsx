eb2fbbb6bcb5067c35d22fccf6086ee6e1cb73f6
Asher Hwang <dev@hsh.kr>
2022-06-13 00:04:54 +0900

    v0.1.1

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,12 @@
+import { VisibilityOff } from '@material-ui/icons';
 import { TextField } from '@mui/material';
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -11,5 +19,41 @@ export const Input: React.FC<{
     <InputContainer>
       <TextField id="standard-basic" label={label} variant="outlined" />
     </InputContainer>
+  );
+};
+
+const EmailInputContainer = styled.div``;
+
+export const EmailInput: React.FC<{
+  label: string;
+}> = ({ label }) => {
+  return (
+    <EmailInputContainer>
+      <TextField id="standard-basic" label={label} variant="outlined" />
+    </EmailInputContainer>
+  );
+};
+
+const PasswordInputContainer = styled.div``;
+
+export const PasswordInput: React.FC<{ label: string }> = ({ label }) => {
+  return (
+    <PasswordInputContainer>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-pass2word">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type="password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton aria-label="toggle password visibility" edge="end">
+                <VisibilityOff />
+              </IconButton>
+            </InputAdornment>
+          }
+          label={label}
+        />
+      </FormControl>
+    </PasswordInputContainer>
   );
 };
